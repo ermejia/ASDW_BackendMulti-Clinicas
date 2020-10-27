4c81909ffd33a6d0ee82df736530d69e48d76bb6
ronald <ottoniel.21@outlook.es>
2020-10-27 16:37:30 -0600

    Metodos crud para StallDao

--- src/gt/com/clinica/clinicamedica/dao/StallDao.java
+++ src/gt/com/clinica/clinicamedica/dao/StallDao.java
@@ -1,71 +1,53 @@
-package com.clinica.clinicamedica.dao;
+package gt.com.clinica.clinicamedica.dao;
 
-import java.util.*;
+import gt.com.clinica.clinicamedica.entity.RoomEntity;
+import gt.com.clinica.clinicamedica.entity.StallEntity;
+import gt.com.clinica.clinicamedica.service.ConectionService;
 
-/**
- * 
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class StallDao implements ICrudStall {
 
-    /**
-     * Default constructor
-     */
-    public StallDao() {
-    }
-
-    /**
-     * 
-     */
-    public void selectAllStalls;
-
-    /**
-     * 
-     */
-    public void addStall;
-
-    /**
-     * 
-     */
-    public void updateStall;
-
-    /**
-     * 
-     */
-    public void deleteStall;
-
-    /**
-     * @return
-     */
+    final String selectAllStalls = "SELECT * FROM Puestos;";
+    @Override
     public List<StallEntity> listAllStalls() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param stall 
-     * @return
-     */
-    public int addStall(StallEntity stall) {
-        // TODO implement here
-        return 0;
-    }
-
-    /**
-     * @param stall 
-     * @return
-     */
-    public int updateStall(StallEntity stall) {
-        // TODO implement here
-        return 0;
-    }
-
-    /**
-     * @param id 
-     * @return
-     */
-    public int deleteAppointment(int id) {
-        // TODO implement here
-        return 0;
+        List<StallEntity> listStall = new LinkedList<>();
+        StallEntity stall ;
+        Connection conexion = null;
+        try{
+            ConectionService con= ConectionService.getInstance();
+            conexion = con.getConnection();
+            PreparedStatement statement = conexion.prepareStatement(selectAllStalls);
+            ResultSet consulta=statement.executeQuery();
+            while (consulta.next()) {
+                stall = new StallEntity();
+                stall.setIdStall(consulta.getInt("IdPuesto"));
+                stall.setStall(consulta.getString("Puesto"));
+                listStall.add(stall);
+            }
+            consulta.close();
+            return listStall;
+        }catch (SQLException e){
+            System.err.println("ERROR FATAL! ");
+            System.err.println(e.getMessage());
+            return null;
+        }finally {
+            try {
+                if(conexion !=null) {
+                    conexion.close();
+                }else{
+                    System.out.println("no hay conexion y no se cierra");
+                    return null;
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
     }
 
 }
