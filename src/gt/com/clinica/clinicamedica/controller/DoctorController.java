6fb537e919bab42638f4d9f30ba2208b46ce50ac
cmaxgt2019 <cmaxgt@antiguais.org>
2020-10-31 18:58:03 -0600

    Se creron los metodos Get y post para Doctor Controller

--- src/gt/com/clinica/clinicamedica/controller/DoctorController.java
+++ src/gt/com/clinica/clinicamedica/controller/DoctorController.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-
 @WebServlet("/crudDoctor")
 
 public class DoctorController extends HttpServlet {
